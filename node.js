9e88b8237a09ae9670e80b6001831778a3554653
r-a-johns <r_a_johns@hotmail.com>
2020-05-23 11:14:41 +0100

    added default page to node.js

--- node.js
+++ node.js
@@ -2,10 +2,13 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
+  if(req.url=="/"){
+    req.url = "/TileDesign.html"
+  }
   fs.readFile(__dirname + req.url, function (err,data) {
     if (err) {
       res.writeHead(404);
-      res.end(JSON.stringify(err));
+      res.end("Can't find that file....");
       return;
     }
     res.writeHead(200);
