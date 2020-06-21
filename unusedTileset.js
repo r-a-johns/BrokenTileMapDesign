f82881b959778618d0b91f250d2f92075fe04f53
r-a-johns <r_a_johns@hotmail.com>
2020-06-21 11:49:44 +0100

    Yet more images

---
+++ unusedTileset.js
@@ -0,0 +1,705 @@
+[
+    {
+        set: "grass",
+        icon: 'grass/grass0-1.png',
+        title: "Grass",
+        tiles: [{
+                id: 'grass0-1',
+                src: 'grass/grass0-1.png',
+                subset: 'grass0',
+            },
+
+            {
+                id: 'grass13-1',
+                src: 'grass/grass13-1.png',
+                subset: 'grass13',
+            },
+            {
+                id: 'grass13-2',
+                src: 'grass/grass13-2.png',
+                subset: 'grass13',
+            },
+            {
+                id: 'grass13-3',
+                src: 'grass/grass13-3.png',
+                subset: 'grass13',
+            },
+            {
+                id: 'grass13-4',
+                src: 'grass/grass13-4.png',
+                subset: 'grass13',
+            },
+            {
+                id: 'grass5-1',
+                src: 'grass/grass5-1.png',
+                subset: 'grass2',
+            },
+            {
+                id: 'grass6-1',
+                src: 'grass/grass6-1.png',
+                subset: 'grass2',
+            },
+            {
+                id: 'grass7-1',
+                src: 'grass/grass7-1.png',
+                subset: 'grass2',
+            },
+            {
+                id: 'grass8-1',
+                src: 'grass/grass8-1.png',
+                subset: 'grass2',
+            },
+            {
+                id: 'grass1-1',
+                src: 'grass/grass1-1.png',
+                subset: 'grass1',
+            },
+            {
+                id: 'grass2-1',
+                src: 'grass/grass2-1.png',
+                subset: 'grass1',
+            },
+            {
+                id: 'grass3-1',
+                src: 'grass/grass3-1.png',
+                subset: 'grass1',
+            },
+            {
+                id: 'grass4-1',
+                src: 'grass/grass4-1.png',
+                subset: 'grass1',
+            },
+            {
+                id: 'grass9-1',
+                src: 'grass/grass9-1.png',
+                subset: 'grass3',
+            },
+            {
+                id: 'grass10-1',
+                src: 'grass/grass10-1.png',
+                subset: 'grass3',
+            },
+            {
+                id: 'grass11-1',
+                src: 'grass/grass11-1.png',
+                subset: 'grass3',
+            },
+            {
+                id: 'grass12-1',
+                src: 'grass/grass12-1.png',
+                subset: 'grass3',
+            },
+        ]
+    },
+    {
+        set: "gray",
+        icon: 'gray/gray0-1.png',
+        title: "Gray and walls",
+        tiles: [{
+                id: 'gray0-1',
+                src: 'gray/gray0-1.png',
+                subset: 'gray0',
+            },
+            {
+                id: 'gray10-1',
+                src: 'gray/gray10-1.png',
+                subset: 'gray10',
+            },
+            {
+                id: 'gray10-2',
+                src: 'gray/gray10-2.png',
+                subset: 'gray10',
+            },
+            {
+                id: 'gray10-3',
+                src: 'gray/gray10-3.png',
+                subset: 'gray10',
+            },
+            {
+                id: 'gray10-4',
+                src: 'gray/gray10-4.png',
+                subset: 'gray10',
+            },
+            {
+                id: 'gray9-1',
+                src: 'gray/gray9-1.png',
+                subset: 'gray9',
+            },
+            {
+                id: 'gray9-2',
+                src: 'gray/gray9-2.png',
+                subset: 'gray9',
+            },
+            {
+                id: 'gray9-3',
+                src: 'gray/gray9-3.png',
+                subset: 'gray9',
+            },
+            {
+                id: 'gray9-4',
+                src: 'gray/gray9-4.png',
+                subset: 'gray9',
+            },
+            {
+                id: 'gray12-1',
+                src: 'gray/gray12-1.png',
+                subset: 'gray12',
+            },
+            {
+                id: 'gray12-2',
+                src: 'gray/gray12-2.png',
+                subset: 'gray12',
+            },
+            {
+                id: 'gray12-3',
+                src: 'gray/gray12-3.png',
+                subset: 'gray12',
+            },
+            {
+                id: 'gray12-4',
+                src: 'gray/gray12-4.png',
+                subset: 'gray12',
+            },
+            {
+                id: 'gray8-1',
+                src: 'gray/gray8-1.png',
+                subset: 'gray8',
+            },
+            {
+                id: 'gray8-2',
+                src: 'gray/gray8-2.png',
+                subset: 'gray8',
+            },
+            {
+                id: 'gray8-3',
+                src: 'gray/gray8-3.png',
+                subset: 'gray8',
+            },
+            {
+                id: 'gray8-4',
+                src: 'gray/gray8-4.png',
+                subset: 'gray8',
+            },
+        ]
+    },
+    {
+        set: "path",
+        icon: 'path/path0-1.png',
+        title: "Path",
+        tiles: [{
+                id: 'path0-1',
+                src: 'path/path0-1.png',
+                subset: 'path0'
+            },
+            {
+                id: 'path1-1',
+                src: 'path/path1-1.png',
+                subset: 'path1'
+            },
+            {
+                id: 'path2-1',
+                src: 'path/path2-1.png',
+                subset: 'path1'
+            },
+            {
+                id: 'path3-1',
+                src: 'path/path3-1.png',
+                subset: 'path1'
+            },
+            {
+                id: 'path4-1',
+                src: 'path/path4-1.png',
+                subset: 'path1'
+            },
+            {
+                id: 'path5-1',
+                src: 'path/path5-1.png',
+                subset: 'path5'
+            },
+            {
+                id: 'path6-1',
+                src: 'path/path6-1.png',
+                subset: 'path5'
+            },
+            {
+                id: 'path7-1',
+                src: 'path/path7-1.png',
+                subset: 'path5'
+            },
+            {
+                id: 'path8-1',
+                src: 'path/path8-1.png',
+                subset: 'path5'
+            },
+            {
+                id: 'path9-1',
+                src: 'path/path9-1.png',
+                subset: 'path6'
+            },
+            {
+                id: 'path10-1',
+                src: 'path/path10-1.png',
+                subset: 'path6'
+            },
+            {
+                id: 'path11-1',
+                src: 'path/path11-1.png',
+                subset: 'path6'
+            },
+            {
+                id: 'path12-1',
+                src: 'path/path12-1.png',
+                subset: 'path6'
+            },
+            {
+                id: 'path13-1',
+                src: 'path/path13-1.png',
+                subset: 'path13'
+            },
+            {
+                id: 'path14-1',
+                src: 'path/path14-1.png',
+                subset: 'path13'
+            },
+            {
+                id: 'path15-1',
+                src: 'path/path15-1.png',
+                subset: 'path13'
+            },
+            {
+                id: 'path16-1',
+                src: 'path/path16-1.png',
+                subset: 'path13'
+            },
+            {
+                id: 'path17-1',
+                src: 'path/path17-1.png',
+                subset: 'path14'
+            },
+            {
+                id: 'path18-1',
+                src: 'path/path18-1.png',
+                subset: 'path14'
+            },
+            {
+                id: 'path19-1',
+                src: 'path/path19-1.png',
+                subset: 'path14'
+            },
+            {
+                id: 'path20-1',
+                src: 'path/path20-1.png',
+                subset: 'path14'
+            },
+            {
+                id: 'path21-1',
+                src: 'path/path21-1.png',
+                subset: 'path21'
+            },
+            {
+                id: 'path22-1',
+                src: 'path/path22-1.png',
+                subset: 'path21'
+            },
+            {
+                id: 'path23-1',
+                src: 'path/path23-1.png',
+                subset: 'path21'
+            },
+            {
+                id: 'path24-1',
+                src: 'path/path24-1.png',
+                subset: 'path21'
+            },
+            {
+                id: 'path25-1',
+                src: 'path/path25-1.png',
+                subset: 'path25'
+            },
+            {
+                id: 'path25-2',
+                src: 'path/path25-2.png',
+                subset: 'path25'
+            },
+            {
+                id: 'path25-3',
+                src: 'path/path25-3.png',
+                subset: 'path25'
+            },
+            {
+                id: 'path25-4',
+                src: 'path/path25-4.png',
+                subset: 'path25'
+            },
+            {
+                id: 'path26-1',
+                src: 'path/path26-1.png',
+                subset: 'path26'
+            },
+            {
+                id: 'path26-2',
+                src: 'path/path26-2.png',
+                subset: 'path26'
+            },
+            {
+                id: 'path26-3',
+                src: 'path/path26-3.png',
+                subset: 'path26'
+            },
+            {
+                id: 'path26-4',
+                src: 'path/path26-4.png',
+                subset: 'path26'
+            },
+        ]
+    },
+    {
+        set: "water",
+        icon: 'water/water9-1.png',
+        title: "Water",
+        tiles: [{
+                id: 'water0-1',
+                src: 'water/water0-1.png',
+                subset: 'water0',
+            },
+            {
+                id: 'water1-1',
+                src: 'water/water1-1.png',
+                subset: 'water1',
+            },
+            {
+                id: 'water2-1',
+                src: 'water/water2-1.png',
+                subset: 'water1',
+            },
+            {
+                id: 'water3-1',
+                src: 'water/water3-1.png',
+                subset: 'water1',
+            },
+            {
+                id: 'water4-1',
+                src: 'water/water4-1.png',
+                subset: 'water1',
+            },
+            {
+                id: 'water5-1',
+                src: 'water/water5-1.png',
+                subset: 'water5',
+            },
+            {
+                id: 'water6-1',
+                src: 'water/water6-1.png',
+                subset: 'water5',
+            },
+            {
+                id: 'water7-1',
+                src: 'water/water7-1.png',
+                subset: 'water5',
+            },
+            {
+                id: 'water8-1',
+                src: 'water/water8-1.png',
+                subset: 'water5',
+            },
+            {
+                id: 'water9-1',
+                src: 'water/water9-1.png',
+                subset: 'water9',
+            },
+            {
+                id: 'water10-1',
+                src: 'water/water10-1.png',
+                subset: 'water9',
+            },
+            {
+                id: 'water11-1',
+                src: 'water/water11-1.png',
+                subset: 'water9',
+            },
+            {
+                id: 'water12-1',
+                src: 'water/water12-1.png',
+                subset: 'water9',
+            },
+        ]
+    },
+    {
+        set: "red",
+        icon: 'red/red1-1.png',
+        title: "Red",
+        tiles: [{
+                id: 'red1-1',
+                src: 'red/red1-1.png',
+                subset: 'red1',
+            },
+            {
+                id: 'red2-2',
+                src: 'red/red2-2.png',
+                subset: 'red2',
+            },
+            {
+                id: 'red2-4',
+                src: 'red/red2-4.png',
+                subset: 'red2',
+            },
+            {
+                id: 'red2-3',
+                src: 'red/red2-3.png',
+                subset: 'red2',
+            },
+            {
+                id: 'red2-1',
+                src: 'red/red2-1.png',
+                subset: 'red2',
+            },
+            {
+                id: 'red3-4',
+                src: 'red/red3-4.png',
+                subset: 'red3',
+            },
+            {
+                id: 'red3-3',
+                src: 'red/red3-3.png',
+                subset: 'red3',
+            },
+            {
+                id: 'red3-2',
+                src: 'red/red3-2.png',
+                subset: 'red3',
+            },
+            {
+                id: 'red3-1',
+                src: 'red/red3-1.png',
+                subset: 'red3',
+            },
+            {
+                id: 'red4-3',
+                src: 'red/red4-3.png',
+                subset: 'red4',
+            },
+            {
+                id: 'red4-2',
+                src: 'red/red4-2.png',
+                subset: 'red4',
+            },
+            {
+                id: 'red4-4',
+                src: 'red/red4-4.png',
+                subset: 'red4',
+            },
+            {
+                id: 'red4-1',
+                src: 'red/red4-1.png',
+                subset: 'red4',
+            },
+            {
+                id: 'red5-2',
+                src: 'red/red5-2.png',
+                subset: 'red5',
+            },
+            {
+                id: 'red5-3',
+                src: 'red/red5-3.png',
+                subset: 'red5',
+            },
+            {
+                id: 'red5-4',
+                src: 'red/red5-4.png',
+                subset: 'red5',
+            },
+            {
+                id: 'red5-1',
+                src: 'red/red5-1.png',
+                subset: 'red5',
+            },
+        ]
+    },
+    {
+        set: "green",
+        icon: 'green/green1-1.png',
+        title: "Green",
+        tiles: [{
+                id: 'green1-1',
+                src: 'green/green1-1.png',
+                subset: 'green1',
+            },
+            {
+                id: 'green2-2',
+                src: 'green/green2-2.png',
+                subset: 'green2',
+            },
+            {
+                id: 'green2-4',
+                src: 'green/green2-4.png',
+                subset: 'green2',
+            },
+            {
+                id: 'green2-3',
+                src: 'green/green2-3.png',
+                subset: 'green2',
+            },
+            {
+                id: 'green2-1',
+                src: 'green/green2-1.png',
+                subset: 'green2',
+            },
+            {
+                id: 'green3-4',
+                src: 'green/green3-4.png',
+                subset: 'green3',
+            },
+            {
+                id: 'green3-3',
+                src: 'green/green3-3.png',
+                subset: 'green3',
+            },
+            {
+                id: 'green3-2',
+                src: 'green/green3-2.png',
+                subset: 'green3',
+            },
+            {
+                id: 'green3-1',
+                src: 'green/green3-1.png',
+                subset: 'green3',
+            },
+            {
+                id: 'green4-3',
+                src: 'green/green4-3.png',
+                subset: 'green4',
+            },
+            {
+                id: 'green4-2',
+                src: 'green/green4-2.png',
+                subset: 'green4',
+            },
+            {
+                id: 'green4-4',
+                src: 'green/green4-4.png',
+                subset: 'green4',
+            },
+            {
+                id: 'green4-1',
+                src: 'green/green4-1.png',
+                subset: 'green4',
+            },
+            {
+                id: 'green5-3',
+                src: 'green/green5-3.png',
+                subset: 'green5',
+            },
+            {
+                id: 'green5-2',
+                src: 'green/green5-2.png',
+                subset: 'green5',
+            },
+            {
+                id: 'green5-4',
+                src: 'green/green5-4.png',
+                subset: 'green5',
+            },
+            {
+                id: 'green5-1',
+                src: 'green/green5-1.png',
+                subset: 'green5',
+            },
+        ]
+    },
+    {
+        set: "blue",
+        icon: "blue/blue0-1.png",
+        title: "Blue",
+        tiles: [{
+                id: 'blue0-1',
+                src: 'blue/blue0-1.png',
+                subset: 'blue1',
+            },
+            {
+                id: 'blue1-1',
+                src: 'blue/blue1-1.png',
+                subset: 'blue2',
+            },
+            {
+                id: 'blue1-2',
+                src: 'blue/blue1-2.png',
+                subset: 'blue2',
+            },
+            {
+                id: 'blue1-3',
+                src: 'blue/blue1-3.png',
+                subset: 'blue2',
+            },
+            {
+                id: 'blue1-4',
+                src: 'blue/blue1-4.png',
+                subset: 'blue2',
+            },
+            {
+                id: 'blue1-1',
+                src: 'blue/blue1-1.png',
+                subset: 'blue3',
+            },
+            {
+                id: 'blue2-4',
+                src: 'blue/blue2-4.png',
+                subset: 'blue3',
+            },
+            {
+                id: 'blue2-3',
+                src: 'blue/blue2-3.png',
+                subset: 'blue3',
+            },
+            {
+                id: 'blue2-2',
+                src: 'blue/blue2-2.png',
+                subset: 'blue3',
+            },
+            {
+                id: 'blue2-1',
+                src: 'blue/blue2-1.png',
+                subset: 'blue3',
+            },
+            {
+                id: 'blue3-3',
+                src: 'blue/blue3-3.png',
+                subset: 'blue4',
+            },
+            {
+                id: 'blue3-2',
+                src: 'blue/blue3-2.png',
+                subset: 'blue4',
+            },
+            {
+                id: 'blue3-4',
+                src: 'blue/blue3-4.png',
+                subset: 'blue4',
+            },
+            {
+                id: 'blue3-1',
+                src: 'blue/blue3-1.png',
+                subset: 'blue4',
+            },
+            {
+                id: 'blue4-3',
+                src: 'blue/blue4-3.png',
+                subset: 'blue5',
+            },
+            {
+                id: 'blue4-4',
+                src: 'blue/blue4-4.png',
+                subset: 'blue5',
+            },
+            {
+                id: 'blue4-2',
+                src: 'blue/blue4-2.png',
+                subset: 'blue5',
+            },
+            {
+                id: 'blue4-1',
+                src: 'blue/blue4-1.png',
+                subset: 'blue5',
+            },
+        ]
+    },
+]
